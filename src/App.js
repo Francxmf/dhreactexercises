d8a49f75c98fd3913cbed7be1ff770e9d59076c6
Francesco M Ferri <franko@MinidiFrancesco.homenet.telecomitalia.it>
2022-09-02 10:40:43 +0200

    completion of the second jsx exercise

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//greetings = (name) => {<h1>Hello, {name}!</h1>};
+
+
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+function helloSomeone(name) {
+  return <h1>Hello, {name}</h1>;
+}
+
+const container = document.querySelector("#root")
+
+const root = ReactDOM.createRoot(container)
+
+root.render(helloSomeone("Ciccio"))
