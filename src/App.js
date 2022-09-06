9de768d99dcce00ac2611b93f34b5efc0477ea6d
Francesco M Ferri <franko@MinidiFrancesco.homenet.telecomitalia.it>
2022-09-06 15:56:30 +0200

    completion of the exercise State01

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Welcome } from "./Welcome";
+import { Counter } from "./Counter";
 
 
 export class App extends React.Component{
@@ -7,6 +8,7 @@ export class App extends React.Component{
       return (
         <div>
           <Welcome name={"Mjr. Sandwich"} age={18} />
+          <Counter />
         </div>
       );
   }
