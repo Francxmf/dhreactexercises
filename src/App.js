08794d001833e21c20c0ccbc9820e87b57c24e96
Francesco M Ferri <franko@MinidiFrancesco.homenet.telecomitalia.it>
2022-09-07 22:02:36 +0200

    completion of the exercise forms 03

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Counter } from "./Counter";
 import { ClickTracker } from "./ClickTracker";
-
-
+import { Login } from "./Login";
 
 
 export class App extends React.Component{
+  onLogin(state){
+    console.log(state)
+  }
   render(){
       return (
         <div>
           <InteractiveWelcome />
           <Counter initialValue={0} />
           <ClickTracker />    
-          
+          <Login passFunc= {this.onLogin}/>
         </div>
       );
   }
---
+++ src/Login.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export class Login extends React.Component {
+  state = {
+    username: "",
+    remember: false,
+    password: "",
+    disabled: true,
+  };
+  loginInputsHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    const checked = event.target.checked;
+    const type = event.target.type;
+    this.setState(
+      {
+        [name]: type === "checkbox" ? checked : value,
+      },
+      () =>
+        this.setState({
+          disabled:
+            this.state.username !== "" && this.state.password !== ""
+              ? false
+              : true,
+        })
+    );
+  };
+  loginButtonHandler = (event) => {
+     this.props.passFunc(this.state)
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          name="username"
+          onChange={this.loginInputsHandler}
+          value={this.state.username}
+        />
+        <input
+          name="password"
+          type="password"
+          onChange={this.loginInputsHandler}
+          value={this.state.password}
+        />
+        <input
+          name="remember"
+          type="checkbox"
+          onChange={this.loginInputsHandler}
+          checked={this.state.remember}
+        />
+        <button disabled={this.state.disabled} onClick={this.loginButtonHandler}>
+          Login
+        </button>
+      </div>
+    );
+  }
+}
