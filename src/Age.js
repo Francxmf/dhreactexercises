e22414cc5ee8b8f7bc35d177860061e920bb3fec
Francesco M Ferri <franko@MinidiFrancesco.homenet.telecomitalia.it>
2022-09-09 12:45:23 +0200

    completion of the exercise forms06, small tweak to the Age component

--- src/Age.js
+++ src/Age.js
@@ -3,7 +3,7 @@ import React from 'react';
 export class Age extends React.Component {
     render(){
         return <div>
-            {this.props.age > 18 ? (<p>Your age is {this.props.age}</p>) : (<p>You are very young!</p>)}
+            {this.props.age > 18 ? (<p>Your are {this.props.age} years old.</p>) : (<p>You are very young!</p>)}
         </div>
     }
 }
--- src/UncontrolledLogin.js
+++ src/UncontrolledLogin.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { createRef } from "react";
 
 export class UncontrolledLogin extends React.Component {
+    _autofocusRef = createRef()
+
   uLoginHandler = (event) => {
     event.preventDefault();
     const username = event.target.elements.username.value;
@@ -10,18 +12,22 @@ export class UncontrolledLogin extends React.Component {
     this.props.passFunction({username,password,remember})
   };
 
+  componentDidMount(){
+    this._autofocusRef.current.focus()
+  }
+
   render() {
     return (
         <div>
-        <h2>User login:</h2>
-      <form onSubmit={this.uLoginHandler}>
-        <input name="username" />
-        <input name="password" type="password" />
-        <input name="remember" type="checkbox" />
-        <button type="submit">Login</button>
-        <button type="reset">Reset</button>
-      </form>
-      </div>
+         <h2>User login:</h2>
+         <form onSubmit={this.uLoginHandler}>
+           <input ref={this._autofocusRef} name="username" />
+           <input name="password" type="password" />
+           <input name="remember" type="checkbox" />
+           <button type="submit">Login</button>
+           <button type="reset">Reset</button>
+         </form>
+        </div>
     );
   }
 }
