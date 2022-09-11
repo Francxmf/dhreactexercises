188408fa9ed872563fbde6816445e8a78b3ad571
Francesco M Ferri <franko@MinidiFrancesco.homenet.telecomitalia.it>
2022-09-11 15:47:45 +0200

    completion of the exercise Styling components 01

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,7 +3,7 @@ import { Age } from "./Age"
 
 export class Welcome extends React.Component{
     render(){
-        return <div>
+        return <div className="welcome">
         <p>Welcome, {this.props.name}!</p>
          {
           <Age age={this.props.age} />  
